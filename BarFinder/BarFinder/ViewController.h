66ac43fdbcfacc5240aa14fac86d583d06c8fa27
Kaloian <ka.valkov81@gmail.com>
2016-02-01 04:28:14 -0800

    Initial

---
+++ BarFinder/BarFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarFinder
+//
+//  Created by Kaloian on 2/1/16.
+//  Copyright © 2016 Kaloian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
